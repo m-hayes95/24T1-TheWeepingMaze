c2ceaf0399da4c25d227a28aaad709b49e31ab8e
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2024-02-16 02:34:56 +0900

    Added player movement

---
+++ Assets/Player Character/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField][Range(0f, 10f)] private float speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+    }
+}
