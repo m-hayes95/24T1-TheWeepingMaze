78b58f9d2efa18d5f047b5b70a6c3fc4dcd063ee
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2024-02-16 03:30:35 +0900

    Added enemy patrol

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public Transform pointA, pointB;
+    [SerializeField] private enum EnemySM { PointA, PointB };
+    [SerializeField] private EnemySM enemySM;
+    private Vector3 direction;
+
+    private void Update()
     {
-        
+        transform.position += direction * speed * Time.deltaTime;
+
+        switch (enemySM)
+        {
+            case EnemySM.PointA:
+                direction = (pointB.position - transform.position);
+                if (Vector3.Distance(pointB.position, transform.position) < 0.1f)
+                { 
+                    StartCoroutine(TimerAtPointB());
+                }
+                break;
+
+            case EnemySM.PointB:
+                direction = (pointA.position - transform.position);
+                if (Vector3.Distance(pointA.position, transform.position) < 0.1f)
+                {
+                    StartCoroutine(TimerAtPointA());
+                }
+                break;    
+
+
+        } 
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator TimerAtPointA()
     {
-        
+        yield return new WaitForSeconds(2);
+        enemySM = EnemySM.PointA;
+    }
+
+    private IEnumerator TimerAtPointB()
+    {
+        yield return new WaitForSeconds(2);
+        enemySM = EnemySM.PointB;
     }
 }
