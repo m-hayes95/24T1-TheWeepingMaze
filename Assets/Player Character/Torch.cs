5d0669bb2813729d978626ba761f65232ec7b166
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2024-03-15 01:46:45 +0900

    Added separate recharge timers for when the battery is draining or fully empty

--- Assets/Player Character/Torch.cs
+++ Assets/Player Character/Torch.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
-    [SerializeField] private GameObject _torch;
-    [SerializeField][Range(0f, 60f)] private float batteryMaxTime;
-    [SerializeField][Range(0f, 10f)] private float rechargeTimer;
-    [SerializeField] private float batteryCurrentTime;
-    private float mouseMovement = 0f;
+    [SerializeField, Tooltip("Add a reference to the torch game object here.")] 
+    private GameObject _torch;
+    [SerializeField][Range(0f, 60f), Tooltip("Set how long the battery will last for.")] 
+    private float batteryMaxTime;
+    [SerializeField][Range(0f, 10f), Tooltip("Set how fast the battery will recharge at (Only used when the battery is fully empty).")] 
+    private float fromEmptyRechargeTimer;
+    
+    [SerializeField, Tooltip("Set the rate of how fast / slow the torch will charge (Except for when charging from empty!). " +
+        "Time in seconds is divided by the set number (E.g. 2 will divide each second in half).")] 
+    private float rechargeRate;
 
+    private float batteryCurrentTime;
+    private bool isTorchCharging = false;
     private bool isTorchOn;
     public bool IsTorchOn
     {
         get { return isTorchOn; }
         set { isTorchOn = value; }
     }
-    private bool isTorchCharging = false;
+    
 
     private void Start()
     {
@@ -32,26 +39,48 @@ public class Torch : MonoBehaviour
             batteryCurrentTime -= Time.deltaTime;
             if (batteryCurrentTime <= 0f)
             {
-                StartCoroutine(ChargeBattery());
+                batteryCurrentTime = 0f;
+                StartCoroutine(ChargeBatteryFromEmpty());
             }
         }
+
+        ChargeBatteryOvertime();
     }
     public void ToggleTorch()
     {
-        _torch.SetActive(!_torch.activeSelf);
-        isTorchOn = !isTorchOn;
+        if (!isTorchCharging)
+        {
+            _torch.SetActive(!_torch.activeSelf);
+            isTorchOn = !isTorchOn;
+        }
     }
 
-    private IEnumerator ChargeBattery()
+    // When the battery dies whilst the torch is on,
+    // the player has to wait until its completely recharged to toggle back on.
+    private IEnumerator ChargeBatteryFromEmpty()
     {
         ToggleTorch();
         isTorchCharging = true;
-        Debug.Log("Battery is Charging...");
+        //Debug.Log("Battery is Charging...");
 
-        yield return new WaitForSeconds(rechargeTimer);
-        batteryCurrentTime = batteryMaxTime;
+        yield return new WaitForSeconds(fromEmptyRechargeTimer);
         isTorchCharging = false;
-        Debug.Log("Battery fully charged");
+        batteryCurrentTime = batteryMaxTime;
+        //Debug.Log("Battery fully charged");
+    }
+
+    // The battery will charge over time when not in use
+    private void ChargeBatteryOvertime()
+    {
+        if (!isTorchOn && !isTorchCharging &&
+            batteryCurrentTime < batteryMaxTime)
+        {
+            batteryCurrentTime += (Time.deltaTime / rechargeRate);
+            if (batteryCurrentTime > batteryMaxTime) 
+            {
+                batteryCurrentTime = batteryMaxTime;
+            }
+        }
     }
 
     public float GetCurrentTorchTime()
