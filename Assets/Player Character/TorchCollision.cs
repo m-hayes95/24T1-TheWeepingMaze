b2aa580ee183e75232f0c512a8371d8527df326e
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2024-03-16 01:51:07 +0900

    Added new torch collider

--- Assets/Player Character/TorchCollision.cs
+++ Assets/Player Character/TorchCollision.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class TorchCollision : MonoBehaviour
 {
+    private Collider collider;
+    private void Start()
+    {
+        collider = GetComponentInChildren<Collider>();
+        if (collider == null)
+        {
+            Debug.LogWarning("No collider found in torch");
+        }
+    }
     // Stop the enemy from moving when they are in the torch light.
     private void OnTriggerStay(Collider collision)
     {
